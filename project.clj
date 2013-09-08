6fbb9d9e2bea6ec7f3228626feecf19591f4c8b9
chapmanb <chapmanb@50mail.com>
2013-09-08 11:31:19 -0400

    Add heavy weight GATK dependency for annotation calling

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "https://github.com/chapmanb/bcbio.variation.plus"
   :license {:name "MIT" :url "http://www.opensource.org/licenses/mit-license.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [bcbio.variation "0.1.0"]]
+                 [bcbio.variation "0.1.0"]
+                 ;; GATK dependencies
+                 [it.unimi.dsi/fastutil "6.5.3"]
+                 ]
   :plugins [[lein-midje "3.0.1"]]
   :profiles {:dev {:dependencies
                    [[midje "1.5.1" :exclusions [org.clojure/clojure ordered]]]}}
