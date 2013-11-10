a84567cb24c740095aa777d9a43b5ae941cfcc81
chapmanb <chapmanb@50mail.com>
2013-11-10 05:58:59 -0500

    Add realignment with glia and recalling using freebayes for a set of union calls

--- test/bcbio/variation/test_erealign.clj
+++ test/bcbio/variation/test_erealign.clj
@@ -21,5 +21,6 @@
            ?form)))
 
 (facts "Calculate ensemble set of variants from multiple inputs using realignment."
-  (let [region {:chrom "10" :start 250000 :end 400000}]
-    (erealign/by-region region vcf-files bam-file ref-file work-dir)))
+  (let [region {:chrom "10" :start 250000 :end 400000}
+        out-file (str (io/file data-dir "work" "10_250000_400000-recall-10_250000_400000.vcf"))]
+    (erealign/by-region region vcf-files bam-file ref-file work-dir) => out-file))
